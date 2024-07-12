61f6642a53aa7f9c2ce4427e9de30395efc5eab5
Cristina Loma <crislaragones@gmail.com>
2024-07-12 16:51:53 +0100

    config del proyecto

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,8 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:edit, :update, :destroy]
   def index
+    @restaurants = Restaurant.all
+    @restaurant = Restaurant.new
   end
 
   def show
@@ -12,11 +15,35 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path, notice: 'Restaurant was successfully created.'
+    else
+      @restaurants = Restaurant.all
+      render :index
+    end
   end
 
   def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurants_path, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path, notice: 'Restaurant was successfully destroyed.'
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
   end
 end
